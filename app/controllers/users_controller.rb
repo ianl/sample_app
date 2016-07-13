6da244d1791b322ab9ad29e45cdbbc13f45a1720
ianthl <ian1654@gmail.com>
2016-07-13 17:54:54 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -49,18 +50,14 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     def correct_user
