fb55e95dcb0e79ca105f673a2665f51d474920ab
ianthl <ian1654@gmail.com>
2016-07-15 20:55:31 +0800

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,8 +50,18 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  def feed
-    Micropost.where("user_id = ?", id)
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
